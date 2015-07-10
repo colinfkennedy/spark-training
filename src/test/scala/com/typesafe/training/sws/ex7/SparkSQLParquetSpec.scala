238131e478a5901a1528fd7364e85062d0fa9648
Nilanjan Raychaudhuri <nraychaudhuri@gmail.com>
2015-07-10 15:14:45 +0200

    Fixed test

--- src/test/scala/com/typesafe/training/sws/ex7/SparkSQLParquetSpec.scala
+++ src/test/scala/com/typesafe/training/sws/ex7/SparkSQLParquetSpec.scala
@@ -15,7 +15,7 @@ class SparkSQLParquetSpec extends FunSpec {
       val fileStream = new FileOutputStream(new File(out))
       SparkSQLParquet.out = new PrintStream(fileStream, true)
 
-      SparkSQLParquet.main(Array("--quiet", "--in", in))
+      SparkSQLParquet.main(Array("--master", "local", "--quiet", "--in", in))
 
       TestUtil.verifyAndClean(out, golden, out)
     }
